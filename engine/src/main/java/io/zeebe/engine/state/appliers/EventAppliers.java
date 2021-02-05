0588426a8bc3039124107fd88274df6b45e82af9
pihme <pihme@users.noreply.github.com>
2021-02-05 16:09:23 +0100

    chore(engine): event sourcing for CompleteJob/JobCompleted

--- engine/src/main/java/io/zeebe/engine/state/appliers/EventAppliers.java
+++ engine/src/main/java/io/zeebe/engine/state/appliers/EventAppliers.java
@@ -43,8 +43,13 @@ public final class EventAppliers implements EventApplier {
     register(
         WorkflowInstanceIntent.ELEMENT_ACTIVATED,
         new WorkflowInstanceElementActivatedApplier(state));
-    register(JobIntent.CREATED, new JobCreatedApplier(state));
     register(WorkflowIntent.CREATED, new WorkflowCreatedApplier(state));
+    registerJobIntentEventAppliers(state);
+  }
+
+  private void registerJobIntentEventAppliers(final ZeebeState state) {
+    register(JobIntent.CREATED, new JobCreatedApplier(state));
+    register(JobIntent.COMPLETED, new JobCompletedEventApplier(state));
   }
 
   private <I extends Intent> void register(final I intent, final TypedEventApplier<I, ?> applier) {
